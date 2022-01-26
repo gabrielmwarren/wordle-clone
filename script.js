2ece97661e210e24cd2b6203f4d43e43174d404a
Gabriel Warren <gwarren@andrew.cmu.edu>
2022-01-26 18:30:30 -0500

    added project

---
+++ script.js
@@ -0,0 +1,187 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createSquares();
+
+    let guessedWords = [[]];
+    let availableSpace = 1;
+    let guessedWordCount = 0;
+
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+    const messageText = document.getElementById("messageText");
+
+    function getNewWord() {
+      let wordNum = Math.floor(Math.random() * (WordList.length - 0 + 1)) + 0;
+      let word = WordList[wordNum];
+      return word;
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    };
+
+    const word = getNewWord();
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+    
+        if (currentWordArr && currentWordArr.length < 5) {
+          currentWordArr.push(letter);
+    
+          const availableSpaceEl = document.getElementById(String(availableSpace));
+    
+          availableSpace = availableSpace + 1;
+          availableSpaceEl.textContent = letter;
+        };
+    };
+    
+    function showMessage(text) {
+      messageText.innerText = text;
+      messageText.classList.add("animate__animated");
+      messageText.classList.add("animate__bounce");
+      messageText.style.display = "block";
+    };
+
+
+    function getTileColor(letter, index,  word) {
+        const isCorrectLetter = word.includes(letter);
+    
+        if (!isCorrectLetter) {
+          for (let i = 0; i < keys.length; i++) {
+            if (letter === keys[i].innerText.toLowerCase()) {
+              keys[i].style.backgroundColor = "rgb(58, 58, 60)";
+            };
+          };
+          return "rgb(58, 58, 60)";
+        };
+    
+        const letterInThatPosition = word.charAt(index);
+        const isCorrectPosition = letter === letterInThatPosition;
+    
+        if (isCorrectPosition) {
+          for (let i = 0; i < keys.length; i++) {
+            if (letter === keys[i].innerText.toLowerCase()) {
+              keys[i].style.backgroundColor = "rgb(83, 141, 78)";
+            };
+          };
+          return "rgb(83, 141, 78)";
+        } else if (isCorrectLetter) {
+          for (let i = 0; i < keys.length; i++) {
+            if (letter === keys[i].innerText.toLowerCase()) {
+              keys[i].style.backgroundColor = "rgb(181, 159, 59)";
+            };
+          };
+          return "rgb(181, 159, 59)";
+        };
+      };
+
+    async function handleSubmitWord() {
+        const currentWordArr  = getCurrentWordArr();
+        if (currentWordArr.length != 5) {
+            window.alert("Must Be Five Letters");
+            return;
+        };
+
+        let res = await fetch(`https://dictionaryapi.com/api/v3/references/sd3/json/${currentWordArr}?key=930fd199-60e3-4396-bc16-9cd4ccc8c4b0`);
+        let response = await res.json();
+        let resOne = response[0]; 
+        try {
+          resOne.def;
+        } catch (error) {
+          showMessage("Not In Word List");
+          return;;
+        };
+
+        const currentWord = currentWordArr.join('');
+
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200;
+        currentWordArr.forEach((letter, index) => {
+          setTimeout(() => {
+            const tileColor = getTileColor(letter, index, word);
+
+            const letterId = firstLetterId + index;
+            const letterEl = document.getElementById(letterId);
+            letterEl.classList.add("animate__flipInX");
+            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+          }, interval * index);
+        });
+
+        guessedWordCount += 1;
+
+        if (currentWord === word) {
+            showMessage("You Win!")
+        };
+
+        if (guessedWords.length === 6) {
+            showMessage("You Lose")
+        };
+
+        guessedWords.push([]);
+    };
+
+    function createSquares() {
+        const gameBoard = document.getElementById("board");
+
+        for (let index = 0; index < 30; index++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.classList.add("animate__animated");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square);
+        };
+    };
+
+    function handleDeleteLetter() {
+        const currentWordArr = getCurrentWordArr();
+        const removedLetter = currentWordArr.pop();
+
+        if (messageText.style.display === "block") {
+          messageText.style.display = "none";
+        }
+    
+        guessedWords[guessedWords.length - 1] = currentWordArr;
+    
+        const lastLetterEl = document.getElementById(String(availableSpace - 1));
+    
+        lastLetterEl.textContent = "";
+        availableSpace = availableSpace - 1;
+      };
+
+    
+    const forLoop = async forLoop => {
+        for (let i = 0; i < keys.length; i++) {
+            keys[i].onclick = ({ target }) => {
+              const letter = target.getAttribute("data-key");
+    
+              if (letter === 'enter') {
+                  handleSubmitWord();
+                  return;
+              };
+
+              if (letter === "del") {
+                handleDeleteLetter();
+                return;
+              };
+    
+              updateGuessedWords(letter);
+            };
+        };
+
+        /* 
+        let res = await fetch("https://dictionaryapi.com/api/v3/references/sd3/json/gh?key=930fd199-60e3-4396-bc16-9cd4ccc8c4b0");
+        let response = await res.json();
+        console.log(response);
+        let resOne = response[0];
+        if (resOne.def) {
+            console.log("That's A Valid Valid Word");
+        } else {
+            console.log("That's Not A Valid Valid Word");
+        };
+        */
+    };
+
+
+
+    forLoop()
+});
